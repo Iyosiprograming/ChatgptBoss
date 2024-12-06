1c564ab844e8f2f4de3b3a16c8a4da0371447af3
Iyosiyas <learningstation0974@gmail.com>
2024-12-06 18:54:23 +0300

    backend started

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,32 @@
 const express = require ("express")
 const mongoose = require ("mongoose")
+const app = express()
+const PORT = 2000
 
+// create a CRUD api that get , post , delete , update users todo backend
 
+app.get("/api/todos",(req,res)=>{
+    res.send("GET Hello world")
+})
+
+app.post("/api/todos",(req,res)=>{
+    res.send("POST Hello world")
+})
+
+app.get("/api/todos/:id",(req,res)=>{
+    res.send("GET hello world By id")
+})
+
+app.put("/api/todos/:id",(req,res)=>{
+    res.send("PUT hello wold")
+})
+
+app.delete("/api/todos/:id",(req,res)=>{
+    res.send("DELETE hello world")
+})
+
+// give listen port
+
+app.listen(PORT,()=>{
+    console.log("Connected ")
+})
